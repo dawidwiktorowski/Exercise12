7e8aa492bf5c39d6bdb36606c4abf1c1468d6ab1
Dawid Wiktorowski <dawid.wiktorowski22@gmail.com>
2022-06-04 16:03:54 +0200

    Initial commit

---
+++ src/test/java/io/github/dawidwiktorowski/exercise12/Exercise12ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.dawidwiktorowski.exercise12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exercise12ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
